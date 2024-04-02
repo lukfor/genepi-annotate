ed7236ad64cbe1de8bdb29fc463401f467943465
Lukas Forer <lukas.forer@i-med.ac.at>
2024-04-02 13:11:13 +0200

    Add header and prepare release 0.1.0

--- genepi-annotate/src/main/java/genepi/annotate/App.java
+++ genepi-annotate/src/main/java/genepi/annotate/App.java
@@ -4,17 +4,33 @@ import java.lang.reflect.InvocationTargetException;
 
 import genepi.base.Toolbox;
 
-/**
- * Hello world!
- *
- */
 public class App extends Toolbox {
 
 	public App(String command, String[] args) {
 		super(command, args);
 	}
 
+	public static final String APP = "genomic-utils";
+
+	public static final String VERSION = "0.1.0";
+
+	public static final String URL = "https://github.com/lukfor/genepi-annotate";
+
+	public static final String COPYRIGHT = "(c) 2016-2024 Lukas Forer";
+
+	public static String[] ARGS = new String[0];
+
 	public static void main(String[] args) {
+
+		System.err.println();
+		System.err.println(APP + " " + VERSION);
+		if (URL != null && !URL.isEmpty()) {
+			System.err.println(URL);
+		}
+		if (COPYRIGHT != null && !COPYRIGHT.isEmpty()) {
+			System.err.println(COPYRIGHT);
+		}
+		System.err.println();
 
 		App tools = new App("jar genepi-annotate.jar", args);
 
